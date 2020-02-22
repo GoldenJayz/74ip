00c1a58aa455dec5506a639bacf30583d358a5af
GoldenJayz <45317852+GoldenJayz@users.noreply.github.com>
2020-02-22 14:21:58 -0500

    Update 74ipgaybot.py

--- 74ipgaybot.py
+++ 74ipgaybot.py
@@ -21,54 +21,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-   if message.content == '-ramranch':
-        em = discord.Embed(description='ramranch')
-        await client.send_message(message.channel,'https://www.youtube.com/watch?v=MADvxFXWvwE')
-        await client.send_message(message.channel, embed=em)
-   if message.content == '-jayboss980isafreakingod':
-        em = discord.Embed(description='CRABRAVE')
-        em.set_image(url='https://media.giphy.com/media/S46AgOqU95FHa/giphy.gif')
-        await client.send_message(message.channel, embed=em)
-   if message.content.startswith('-8ball'):
-        randomlist = ['Maybe','Yes','No',]
-        await client.send_message(message.channel,(random.choice(randomlist)))
-   if message.content == '-creator':
-        await client.send_message(message.channel,'@Jaden#3147')
-   if message.content == '-meme':
-        randomlist = ['https://i.ytimg.com/vi/Unxy0tPrxjg/maxresdefault.jpg','Yes','No',]
-   if ('penis') in message.content:
+    if (' ') in message.content:
         await client.delete_message(message)    
         await client.send_message(message.channel,'Do not swear! Mods/Admins have been alerted! <@%s>'  %(message.author.id))
-   if ('shit') in message.content:
-        await client.delete_message(message)    
-        await client.send_message(message.channel,'Do not swear! Mods/Admins have been alerted! <@%s>'  %(message.author.id))
-   if ('nigger') in message.content:
-        await client.delete_message(message)    
-        await client.send_message(message.channel,'Do not swear! Mods/Admins have been alerted! <@%s>'  %(message.author.id))
-   if ('nigga') in message.content:
-        await client.delete_message(message)    
-        await client.send_message(message.channel,'Do not swear! Mods/Admins have been alerted! <@%s>'  %(message.author.id))
-   if ('fuck') in message.content:
-        await client.delete_message(message)    
-        await client.send_message(message.channel,'Do not swear! Mods/Admins have been alerted! <@%s>'  %(message.author.id))
-   if ('bitch') in message.content:
-        await client.delete_message(message)    
-        await client.send_message(message.channel,'Do not swear! Mods/Admins have been alerted! <@%s>'  %(message.author.id))
-   if ('prick') in message.content:
-        await client.delete_message(message)    
-        await client.send_message(message.channel,'Do not swear! Mods/Admins have been alerted! <@%s>'  %(message.author.id))
-   if ('bastard') in message.content:
-        await client.delete_message(message)    
-        await client.send_message(message.channel,'Do not swear! Mods/Admins have been alerted! <@%s>'  %(message.author.id))
-   if ('ass') in message.content:
-        await client.delete_message(message)    
-        await client.send_message(message.channel,'Do not swear! Mods/Admins have been alerted! <@%s>'  %(message.author.id))
-   if ('asshole') in message.content:
-        await client.delete_message(message)    
-        await client.send_message(message.channel,'Do not swear! Mods/Admins have been alerted! <@%s>'  %(message.author.id))
-   if message.content == ('-74ip'):
-        if message.author.id == "641772453168676886":
-            await client.send_message(message.channel,'Hi queen 74ip how may I serve you?')
-        else:
-            await client.send_message(message.channel, "Only the owner of this bot can access this command. Sorry!")
 client.run(str(os.environ.get('BOT_TOKEN')))
